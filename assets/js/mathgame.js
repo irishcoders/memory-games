f92c8ae532440ab27aa91d339cd0f675cd159e12
irishcoders <>
2023-10-20 20:37:08 +0000

    add id to countdown number of questions for math game, add function to shuffle between the operands in the display question, add function to restart or quit game after user completes a round of 10 questions

--- assets/js/mathgame.js
+++ assets/js/mathgame.js
@@ -7,61 +7,45 @@ document.addEventListener("DOMContentLoaded", function () {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
-                let gameType = this.getAttribute("data-type");
-                runGame(gameType);
+                runGame();
             }
         });
     }
 
-    // This adds a listener to the Enter key on the keyboard so that answers can be submitted when the Enter key is pressed
     document.getElementById("math-answer-box").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
     });
 
-    runGame("addition");
-
+    runGame();
 });
 
-let totalQuestions = 25; // Set the total number of questions
+let totalQuestions = 10;
+let correctAnswers = 0;
+let incorrectAnswers = 0;
 
-let correctAnswers = 0; // Initialize the correct answers counter
+function decrementTotalQuestions() {
+    totalQuestions--;
+    document.getElementById("total-questions").innerText = totalQuestions;
+}
 
-/**
- * The main game "loop" will be called when the script is first loaded
- * and after the user's answer has been processed
- */
-function runGame(gameType) {
-    if (correctAnswers >= totalQuestions) {
-        // Check if the user has answered all questions
-        const userScore = correctAnswers + " out of " + totalQuestions;
-        if (correctAnswers >= 20) {
-            alert`Congratulations! You passed. You scored ${correctAnswers} out of ${totalQuestions}`;
-        } else {
-            alert("Sorry, you failed. You scored " + userScore);
-        }
-        const restart = confirm("Would you like to restart the game or quit?");
-        if (restart) {
-            correctAnswers = 0; // Reset the correct answers counter
-            document.getElementById("math-score").innerText = correctAnswers;
-            document.getElementById("math-incorrect").innerText = 0; // Reset incorrect answers counter
-        } else {
-            const quitMessage = "You have chosen to quit the game. Goodbye!";
-            alert(quitMessage);
-            // Redirect the user to another page (example: go to the homepage)
-            window.location.href = "index.html#home"; // Replace "index.html" with the URL you want to redirect to
-        }
+function runGame() {
+    // Randomly select a game type
+    const gameTypes = ["addition", "subtract", "multiply"];
+    const randomIndex = Math.floor(Math.random() * gameTypes.length);
+    const gameType = gameTypes[randomIndex];
+
+    if (totalQuestions === 0) {
+        showResultAlert();
     } else {
-        // This sets the answer box to empty after each game is played and recorded
         document.getElementById("math-answer-box").value = "";
-
-        // This lets the cursor stay in the answer box rather than the user clicking in it to type an answer
         document.getElementById("math-answer-box").focus();
 
-        // This creates random numbers between 1 and 25
         let num1 = Math.floor(Math.random() * 25) + 1;
         let num2 = Math.floor(Math.random() * 25) + 1;
+
+        decrementTotalQuestions(); // Decrement total questions when a new question is displayed
 
         if (gameType === "addition") {
             displayAdditionQuestion(num1, num2);
@@ -76,14 +60,10 @@ function runGame(gameType) {
     }
 }
 
-/**
- * Checks the answer against the correct answer
- * Also checks if the user submits without an input and displays an error message
- */
 function checkAnswer() {
-    let userAnswer = document.getElementById("math-answer-box").value.trim(); // Trim the input
+    let userAnswer = document.getElementById("math-answer-box").value.trim();
     if (userAnswer === "") {
-        alert("Invalid input. Please enter your answer first, then press the Enter key or click Submit Answer to continue.");
+        alert("Invalid input. Please enter your answer first.");
     } else {
         let parsedAnswer = parseInt(userAnswer);
         let calculatedAnswer = calculateCorrectAnswer();
@@ -91,18 +71,13 @@ function checkAnswer() {
             alert("Congrats! You got the correct answer");
             incrementScore();
         } else {
-            alert(`Aww.. Your answer was wrong, your answer was ${parsedAnswer}, but the correct answer is ${calculatedAnswer[0]}!`);
+            alert(`Aww.. Your answer was wrong. The correct answer is ${calculatedAnswer[0]}`);
             incrementWrongAnswer();
         }
-        correctAnswers++;
-        runGame(calculatedAnswer[1]);
+        runGame();
     }
 }
 
-/**
- * Gets the operands (the random numbers) and operator (addition, subtraction, multiplication)
- * directly from the DOM and returns the correct answer
- */
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById("operand1").innerText);
     let operand2 = parseInt(document.getElementById("operand2").innerText);
@@ -115,25 +90,21 @@ function calculateCorrectAnswer() {
     } else if (operator === "*") {
         return [operand1 * operand2, "multiply"];
     } else {
-        alert(`Unimplemented operator ${operator} `);
+        alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
 
-/** 
- * Gets the score from the DOM and increments it by 1, then displays it in the score box
- */
 function incrementScore() {
     let oldScore = parseInt(document.getElementById('math-score').innerText);
     document.getElementById('math-score').innerText = ++oldScore;
+    correctAnswers++;
 }
 
-/** 
- * Gets the incorrect score from the DOM and increments it by 1, then displays it in the incorrect box
- */
 function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById('math-incorrect').innerText);
     document.getElementById('math-incorrect').innerText = ++oldScore;
+    incorrectAnswers++;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -152,4 +123,27 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "*";
+}
+
+function showResultAlert() {
+    if (correctAnswers >= 6) {
+        alert(`Congratulations! You passed. You scored ${correctAnswers} out of 10`);
+    } else {
+        alert(`Sorry, you failed. You scored ${correctAnswers} out of 10`);
+    }
+
+    const restart = confirm("Click 'OK' to Restart or 'Cancel' to Quit Game");
+    if (restart) {
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        totalQuestions = 10; // Reset the total questions count
+        document.getElementById("math-score").innerText = correctAnswers;
+        document.getElementById("math-incorrect").innerText = incorrectAnswers;
+        document.getElementById("total-questions").innerText = totalQuestions; // Update the total questions element
+        runGame();
+    } else {
+        const quitMessage = "You have chosen to quit the game. Goodbye!";
+        alert(quitMessage);
+        window.location.href = "index.html#home";
+    }
 }
